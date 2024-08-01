68bcbde00d0c5c29d061f37ffb463ed2daccffe6
= <=>
2024-08-01 23:47:45 +0530

    first commit

---
+++ Day1.py
@@ -0,0 +1,26 @@
+# print("Hello World!")
+
+# Challange 1 Print desired output to the console
+# print('Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.\ne.g. print("Hello " + "World")\nNew lines can be created with a backslash and n')
+
+# Challange 2 Ask user its name and greet them back
+# name = input("What is your name: ")
+# print("Hello ",name)
+
+# Challange 3 Print the length of name user gives as input
+# name = input("What is your name: ")
+# print("Number of letters in your name are: ",len(name))
+
+# Challange 4 Swap two integers/numbers
+# a = int(input("a = "))
+# b = int(input("b = "))
+# a = a+b
+# b = a-b
+# a = a-b
+# print("a =",a,"\nb =",b)
+
+# Day 1 Project Band Name Generator
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be",city,pet_name)
