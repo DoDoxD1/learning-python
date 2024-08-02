2d5dd3d5528032d21496d1cdc100f18b01a9fff8
= <=>
2024-08-02 08:46:04 +0530

    Day 2 Challange 2

--- Day2.py
+++ Day2.py
@@ -5,8 +5,13 @@
 # Float (1.23, 3.14)
 # Boolean (t]True, False)
 
-# Challange 1
-num = int(input("Enter a two digit number: "))
-tens_place = num//10
-ones_place = num - tens_place*10
-print("The sum of the digits  is: ",(ones_place+tens_place))
+# Challange 1 Add the digits of a two digit number.
+# num = int(input("Enter a two digit number: "))
+# tens_place = num//10
+# ones_place = num - tens_place*10
+# print("The sum of the digits  is: ",(ones_place+tens_place))
+
+# Challange 2 BMI Calculator
+height = float(input("What is your height in m: "))
+weight = float(input("What is your weight in kg: "))
+print("Your BMI: ",int(weight/height**2))
