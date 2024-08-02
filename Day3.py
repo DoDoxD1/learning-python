8abe988239e3880ceabae4201d11d361dc02298c
= <=>
2024-08-02 12:39:42 +0530

    Day 3 Challange 5

--- Day3.py
+++ Day3.py
@@ -30,14 +30,26 @@
 #     print("It's not leap year")
 
 #Challange 4 Pizza order bill
-print("Welcome to my pizza shop!")
-size = input("What is the size of pizza you want? S, M, or L ")
-pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
-bill = 0
-if(size == "S"): bill += 15
-elif(size == "M"): bill += 20
-else: bill += 25
-if(pepperoni == "Y"): bill += 2
-if(extra_cheese == "Y"): bill += 3
-print(f"Your bill is {bill}, Thank you for your order!")
+# print("Welcome to my pizza shop!")
+# size = input("What is the size of pizza you want? S, M, or L ")
+# pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+# bill = 0
+# if(size == "S"): bill += 15
+# elif(size == "M"): bill += 20
+# else: bill += 25
+# if(pepperoni == "Y"): bill += 2
+# if(extra_cheese == "Y"): bill += 3
+# print(f"Your bill is {bill}, Thank you for your order!")
+
+#Challange 5 Love calculator
+name1 = input("Enter your name: ").lower()
+name2 = input("Enter their name: ").lower()
+name = name1+name2
+tens_place = name.count("t")+name.count("r")+name.count("u")+name.count("e")
+ones_place = name.count("l")+name.count("o")+name.count("v")+name.count("e")
+love = int(str(tens_place)+str(ones_place))
+msg = ""
+if(love>90 or love<10): msg = "you go together like coke and mentos"
+elif(love>40 and love<50): msg = "you are alright together"
+print(f"Your love is {love} out of 100, {msg}")
