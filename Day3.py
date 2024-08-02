a35e0dd7a42497090ed46c7597a296977468c3f8
= <=>
2024-08-02 12:27:12 +0530

    Day 3 Challange 4

--- Day3.py
+++ Day3.py
@@ -23,9 +23,21 @@
 # print(f"Your BMI is {bmi}, you are {msg}")
 
 #Challange 3 check for leap year
-year = int(input("Enter the year: "))
-if(year%400==0 or (year%4==0 and year%100!=0)):
-    print("It's a leap year")
-else:
-    print("It's not leap year")
+# year = int(input("Enter the year: "))
+# if(year%400==0 or (year%4==0 and year%100!=0)):
+#     print("It's a leap year")
+# else:
+#     print("It's not leap year")
 
+#Challange 4 Pizza order bill
+print("Welcome to my pizza shop!")
+size = input("What is the size of pizza you want? S, M, or L ")
+pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if(size == "S"): bill += 15
+elif(size == "M"): bill += 20
+else: bill += 25
+if(pepperoni == "Y"): bill += 2
+if(extra_cheese == "Y"): bill += 3
+print(f"Your bill is {bill}, Thank you for your order!")
