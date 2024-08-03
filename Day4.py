1019dd287384b66b5484ccc6c0b1028423c7c9d9
= <=>
2024-08-03 11:37:24 +0530

    day 4 project

--- Day4.py
+++ Day4.py
@@ -19,12 +19,54 @@ import random
 # print(f"{random_choice} is going to pay the bill.")
 
 # Challange 3 mark the treasure
-row1 = ["⬜","⬜","⬜"]
-row2 = ["⬜","⬜","⬜"]
-row3 = ["⬜","⬜","⬜"]
-map = [row1,row2,row3]
-col_row = input("Where do you want to put the treasure? ")
-col = int(col_row[0])-1
-row = int(col_row[1])-1
-map[row][col] = " X"
-print(f"{map[0]}\n{map[1]}\n{map[2]}")
+# row1 = ["⬜","⬜","⬜"]
+# row2 = ["⬜","⬜","⬜"]
+# row3 = ["⬜","⬜","⬜"]
+# map = [row1,row2,row3]
+# col_row = input("Where do you want to put the treasure? ")
+# col = int(col_row[0])-1
+# row = int(col_row[1])-1
+# map[row][col] = " X"
+# print(f"{map[0]}\n{map[1]}\n{map[2]}")
+
+# Project rock, paper, scissors
+# Rock
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+# Paper
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+# Scissors
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+rpc = [rock,paper,scissors]
+user = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+print(rpc[user])
+computer = random.randint(0,2)
+print(f"Computer chose:\n{rpc[computer]}")
+if((user==0 and computer==2) or (user==2 and computer==1) or (user==1 and computer==0)):
+    print("You win!")
+elif((user==2 and computer==0) or (user==1 and computer==2) or (user==0 and computer==1)):
+    print("You loose.")
+else:
+    print("It's a draw.")
