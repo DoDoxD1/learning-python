b8504a38300f907582d911654f2a3fee8f61442c
= <=>
2024-08-03 19:52:20 +0530

    Day 5 project

--- Day5.py
+++ Day5.py
@@ -23,8 +23,31 @@
 # print(sum)
 
 # Challange 4 fizzbzz from 1to100
-for n in range(1,101):
-    if(n%15==0): print("FizzBuzz")
-    elif(n%3==0): print("Fizz")
-    elif(n%5==0): print("Buzz")
-    else: print(n)
+# for n in range(1,101):
+#     if(n%15==0): print("FizzBuzz")
+#     elif(n%3==0): print("Fizz")
+#     elif(n%5==0): print("Buzz")
+#     else: print(n)
+
+# Project password generator
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+n_letters= int(input("How many letters would you like in your password?\n")) 
+n_symbols = int(input(f"How many symbols would you like?\n"))
+n_numbers = int(input(f"How many numbers would you like?\n"))
+password = ""
+
+for i in range(0,n_letters):
+    if(n_numbers!=0): 
+        password += numbers[random.randint(0,len(numbers)-1)]
+        n_numbers -= 1
+    elif(n_symbols!=0): 
+        password += symbols[random.randint(0,len(symbols)-1)]
+        n_symbols -= 1
+    else: password += letters[random.randint(0,len(letters)-1)]
+password = ''.join(random.sample(password,len(password)))
+print(password)
