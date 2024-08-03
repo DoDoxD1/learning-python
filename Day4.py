46e25df26462a63ff65b2c9e2950df6365519317
= <=>
2024-08-03 10:59:38 +0530

    Day 4 Challange 3

--- Day4.py
+++ Day4.py
@@ -11,9 +11,20 @@ import random
 # else: print("Tails")
 
 # Challange 2 banker roulette
-names_str = input("Give me everybody's names, seperated by a comma: ")
-names = names_str.split(", ")
-# random_choice = names[random.randint(1,len(names)-1)]
-# or
-random_choice = random.choice(names)
-print(f"{random_choice} is going to pay the bill.")
+# names_str = input("Give me everybody's names, seperated by a comma: ")
+# names = names_str.split(", ")
+# # random_choice = names[random.randint(1,len(names)-1)]
+# # or
+# random_choice = random.choice(names)
+# print(f"{random_choice} is going to pay the bill.")
+
+# Challange 3 mark the treasure
+row1 = ["⬜","⬜","⬜"]
+row2 = ["⬜","⬜","⬜"]
+row3 = ["⬜","⬜","⬜"]
+map = [row1,row2,row3]
+col_row = input("Where do you want to put the treasure? ")
+col = int(col_row[0])-1
+row = int(col_row[1])-1
+map[row][col] = " X"
+print(f"{map[0]}\n{map[1]}\n{map[2]}")
