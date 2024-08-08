43c16f10369f3443966ae6a09a668d149cea666c
= <=>
2024-08-08 16:44:13 +0530

    Day 17 quiz game completed

---
+++ Day17.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+
+
+user = User("Arihant")
+print(user.name)
+
+# quiz game project
+# https://github.com/DoDoxD1/quiz-game-python
