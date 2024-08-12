29f09a6853ad9138bf1875316905aa31a3ba1782
= <=>
2024-08-12 15:06:06 +0530

    Day 24 complete

--- Day24.py
+++ Day24.py
@@ -1,5 +1,13 @@
 # file system
 
+# file = open("my_file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+with open("my_file.txt") as file:
+    content = file.read()
+    print(content)
 
 # snake game highscore feature and lives feature
 # https://github.com/DoDoxD1/snake-game-python
